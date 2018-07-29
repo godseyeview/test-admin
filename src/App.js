935f95e793f7ccfde65e7e099e64b2d2313374fa
godseyeview <godseyeview@gmail.com>
2018-07-29 13:15:39 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// in src/App.js
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { PostList, PostEdit, PostCreate } from './posts';
+import { UserList } from './users';
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+        <Resource name="users" list={UserList} />
+    </Admin>
+);
+
+export default App;
